445e0002b1287355117fbe771169c19c2cd4213e
Doomsdhg <tohanknv@yandex.ru>
2022-02-25 18:21:15 +0300

    angular material table implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild  } from '@angular/core';
+import transactions from './transactions.json';
+import { DataSource } from '@angular/cdk/table';
+
+const transactionsData = transactions;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'internship-project';
+  displayedColumns: string[] = ['externalId', 'username', 'amount', 'comissionAmount', 'provider'];
+  transactionsArray =  transactionsData;
+  columnNames = [{
+      id: 'externalId',
+      value: 'No.',
+    }, 
+    {
+      id: 'username',
+      value: 'Username',
+    },
+    {
+      id: 'amount',
+      value: 'Amount',
+    },
+    {
+      id: 'comissionAmount',
+      value: 'Comission amount',
+    },
+    {
+      id: 'provider',
+      value: 'Provider',
+    }];
 }
