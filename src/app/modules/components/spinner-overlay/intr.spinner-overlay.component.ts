946ee227fdd47b4dba196f56fd90e36e293789ba
Doomsdhg <tohanknv@yandex.ru>
2022-05-10 18:37:19 +0300

    INTR-01: spinner implemented

---
+++ src/app/modules/components/spinner-overlay/intr.spinner-overlay.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'intr-spinner-overlay',
+  templateUrl: './intr.spinner-overlay.component.html',
+  styleUrls: ['./intr.spinner-overlay.component.scss']
+})
+export class SpinnerOverlayComponent {}
