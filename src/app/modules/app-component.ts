53dadf5742a77aff0dde94a93170f85fb9330dcc
Doomsdhg <80508769+Doomsdhg@users.noreply.github.com>
2022-05-25 17:06:20 +0300

    INTR-05: theme switching reworked from native html interaction to sep… (#23)
    * INTR-05: theme switching reworked from native html interaction to separate service

--- src/app/modules/app-component.ts
+++ src/app/modules/app-component.ts
@@ -1,4 +1,5 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ThemeManagerService } from './../services/theme-manager.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,21 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
   styleUrls: ['app-component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
 
   public title = 'internship-project';
+
+  public currentTheme!: string;
+
+  constructor(private themeManagerService: ThemeManagerService) {}
+
+  public ngOnInit(): void {
+    this.subscribeToThemeChanges();
+  }
+
+  private subscribeToThemeChanges(): void {
+    this.themeManagerService.themeSubject.asObservable().subscribe((newTheme) => {
+      this.currentTheme = newTheme;
+    });
+  }
 }
